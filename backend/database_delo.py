422bda1f1a73f512974514af0e73ad31b92d5418
jpetzen <75584220+jpetzen@users.noreply.github.com>
2024-05-08 00:03:59 +0200

    Update database_delo.py

--- backend/database_delo.py
+++ backend/database_delo.py
@@ -11,7 +11,6 @@ Base: DeclarativeBase = declarative_base()
 
 class Uporabniki(Base):
     __tablename__ = "users_tabela"
-
     id = Column(Integer, primary_key=True, autoincrement=True)
     username = Column(String, index=True, unique=True)
     password = Column(String)
@@ -21,21 +20,18 @@ class Uporabniki(Base):
 
 class OpravilaSeznam(Base):
     __tablename__ = "opravila_tabela"
-    
     id_opravilo = Column(String, primary_key=True)
     evidenca = relationship("Evidenca", back_populates="opravila")
 
 
 class CistilaSeznam(Base):
     __tablename__ = "cistila_tabela"
- 
     id_cistilo = Column(String, primary_key=True)
     sredstva = relationship("Sredstva", back_populates="cistila")
 
 
 class Sredstva(Base):
     __tablename__ = "sredstva_tabela"
-    
     id_sredstva = Column(Integer, primary_key=True, autoincrement=True)
     user_username = Column(String)
     cistilo = Column(String, ForeignKey("cistila_tabela.id_cistilo"))
@@ -44,20 +40,19 @@ class Sredstva(Base):
     denar = Column(Float)
     date = Column(DateTime, default=datetime.utcnow)
 
+
 class Evidenca(Base):
     __tablename__ = "evidenca_tabela"
-    
     id_evidenca = Column(Integer, primary_key=True, autoincrement=True)
     user_username = Column(String)
     done = Column(Boolean)
     datum = Column(DateTime, default=datetime.utcnow)
-    opravilo  = Column(String, ForeignKey("opravila_tabela.id_opravilo"))
+    opravilo = Column(String, ForeignKey("opravila_tabela.id_opravilo"))
     opravila = relationship("OpravilaSeznam", back_populates="evidenca")
 
 
 class ResetToken(Base):
     __tablename__ = "reset_tokens"
-
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     token = Column(String)
     expiration_time = Column(DateTime)
